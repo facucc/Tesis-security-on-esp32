c75bb9a315be6bf4eb3b21d5b86a60613efd9f6c
facucc <facundocarrizo68@gmail.com>
2024-03-17 23:11:00 -0300

    chore: add resources for aws iot job service manager (#6)
    * chore: add resources for aws iot job service manager
    
    * chore: add backend for terraform state

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,16 +1,4 @@
 output "client_id" {
-  value = aws_iot_thing.iot_esp_32.default_client_id
-}
-
-output "cert_pem" {
-  value = aws_iot_certificate.iot_certificate.certificate_pem
-}
-
-output "public_key" {
-  value = aws_iot_certificate.iot_certificate.public_key
-}
-
-output "private_key" {
-  value = aws_iot_certificate.iot_certificate.private_key
-  sensitive = true
+  description = "Cliend ID"
+  value       = aws_iot_thing.iot_esp_32.default_client_id
 }
