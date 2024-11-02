b022b825623c4b86a9e13b2d6b341bb3dd9db5f8
craftech.facuc <facundo.carrizo@craftech.io>
2024-11-02 19:30:56 -0300

    chore: add main component

--- main/main.c
+++ main/main.c
@@ -6,25 +6,54 @@
 
 #include "mqtt_agent.h"
 #include "ota_agent.h"
+#include "cert_renew_agent.h"
+#include "application.h"
+#include "gen_csr.h"
 
-
-#define STACK_SIZE_OTA 6400U
 #define WORD 4
 
-StackType_t xStackOTA_Agent[ STACK_SIZE_OTA * WORD ];
-StaticTask_t xTaskOTABuffer;
 
-#define STACK_SIZE_MQTT 4000U
+#define STACK_SIZE_MQTT 8600U
+#define MQTT_AGENT_TASK_NAME "mqtt_agent"
 
 StackType_t xStackMQTT_Agent[ STACK_SIZE_MQTT * WORD ];
 StaticTask_t xTaskMQTTBuffer;
 
+#define STACK_SIZE_APP 3000U
+#define APP_TASK_NAME "app"
+
+StackType_t xStackApp[ STACK_SIZE_APP * WORD ];
+StaticTask_t xTaskAppBuffer;
+
+#define STACK_SIZE_RENEW 9000U
+#define RENEW_AGENT_TASK_NAME "renew_agent"
+
+StackType_t xStackRenew_Agent[ STACK_SIZE_RENEW * WORD ];
+StaticTask_t xTaskRenewBuffer;
+
+#define STACK_SIZE_OTA 6000U
+#define OTA_AGENT_TASK_NAME "ota_agent"
+
+StackType_t xStackOTA_Agent[ STACK_SIZE_OTA * WORD ];
+StaticTask_t xTaskOTABuffer;
+
+
+#define STACK_SIZE_OTA 8000U
+#define OTA_AGENT_TASK_NAME "ota_agent"
+
+StackType_t xStackOTA_Agent[ STACK_SIZE_OTA * WORD ];
+StaticTask_t xTaskOTABuffer;
+
+static BaseType_t prvWaitForSync( uint32_t * pulNotifiedValue );
+
+/* cppcheck-suppress unusedFunction */
 void app_main()
 {
+  uint32_t ulNotificationValue = 0;
   TaskHandle_t xHandle = NULL;
 
-  xHandle = xTaskCreateStatic( (void *) mqttAgenteTask,
-                              "mqtt agent Task",
+  xHandle = xTaskCreateStatic( (void *) mqttAgentTask,
+                              MQTT_AGENT_TASK_NAME,
                               STACK_SIZE_MQTT,
                               NULL,
                               tskIDLE_PRIORITY,
@@ -33,12 +62,26 @@ void app_main()
 
   if( xHandle == NULL )
   {
-      ESP_LOGE( "main","Failed to create mqtt agent Task:" );
+      ESP_LOGE( "main","Failed to create mqtt agent Task" );
   }
 
-  vTaskDelay( pdMS_TO_TICKS(20000) );
-  xHandle = xTaskCreateStatic( (void *) otaAgenteTask,
-                              "ota agent Task",
+  prvWaitForSync(&ulNotificationValue);
+
+  xHandle = xTaskCreateStatic( (void *) applicationTask,
+                              APP_TASK_NAME,
+                              STACK_SIZE_APP,
+                              NULL,
+                              tskIDLE_PRIORITY,
+                              xStackApp,
+                              &xTaskAppBuffer);
+
+  if( xHandle == NULL )
+  {
+      ESP_LOGE( "main","Failed to create application Task" );
+  }
+
+  xHandle = xTaskCreateStatic( (void *) otaAgentTask,
+                              OTA_AGENT_TASK_NAME,
                               STACK_SIZE_OTA,
                               NULL,
                               tskIDLE_PRIORITY,
@@ -47,7 +90,32 @@ void app_main()
 
   if( xHandle == NULL )
   {
-      ESP_LOGE( "main","Failed to create otaAgenteTask:" );
+      ESP_LOGE( "main","Failed to create otaAgenteTask" );
   }
-  
+
+  xHandle = xTaskCreateStatic( (void *) renewAgentTask,
+                              RENEW_AGENT_TASK_NAME,
+                              STACK_SIZE_RENEW,
+                              NULL,
+                              tskIDLE_PRIORITY,
+                              xStackRenew_Agent,
+                              &xTaskRenewBuffer);
+
+  if( xHandle == NULL )
+  {
+    ESP_LOGE( "main","Failed to create renew agent Task" );
+  }
+
+}
+
+/*-----------------------------------------------------------*/
+static BaseType_t prvWaitForSync( uint32_t * pulNotifiedValue )
+{
+    BaseType_t xReturn;
+
+    /* Wait for this task to get notified, passing out the value it gets
+     * notified with. */
+    xReturn = xTaskNotifyWait( 0, 0, pulNotifiedValue, portMAX_DELAY);
+    
+    return xReturn;
 }
