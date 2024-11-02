02bfd3326a79e136969351b80bdb92f5437863b3
craftech.facuc <facundo.carrizo@craftech.io>
2024-11-02 19:31:43 -0300

    chore: add helper script

--- flash.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-esptool.py -p /dev/ttyUSB0 -b 460800 --before default_reset --after hard_reset --chip esp32 write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x20000 ./build/tesis.bin 0x1000 ./build/bootloader/bootloader.bin 0x8000 ./build/partition_table/partition-table.bin 0x10000 ./nvs/factory_nvs.bin
